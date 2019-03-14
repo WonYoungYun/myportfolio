a00556006b18b83f715ee203acc353cbe5910379
WonYoungYun <asmar20288@naver.com>
2019-03-14 22:40:03 +0900

    Updates

---
+++ precache-manifest.251301dfad57d2e8c60da53d4daa9095.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef000e5539a4737d85791899f7e58276",
+    "url": "/myportfolio/static/media/portfolio.ef000e55.png"
+  },
+  {
+    "revision": "ce2716867f035b952813b224bffa915d",
+    "url": "/myportfolio/static/media/mev.ce271686.png"
+  },
+  {
+    "revision": "7d352db8732233accb83ae3297bed816",
+    "url": "/myportfolio/static/media/jquery.7d352db8.png"
+  },
+  {
+    "revision": "79f689ee44597476bf4c7b13de8ce538",
+    "url": "/myportfolio/static/media/Profile.79f689ee.png"
+  },
+  {
+    "revision": "541058d6d91825ac973653dd14203a67",
+    "url": "/myportfolio/static/media/Home.541058d6.png"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "1a866c3a2d580c656169",
+    "url": "/myportfolio/static/js/main.f2e870e0.chunk.js"
+  },
+  {
+    "revision": "4ef2d503db156fca5840",
+    "url": "/myportfolio/static/js/2.32271690.chunk.js"
+  },
+  {
+    "revision": "e30721b318786ec5f42727cc6205e13c",
+    "url": "/myportfolio/index.html"
+  }
+];
